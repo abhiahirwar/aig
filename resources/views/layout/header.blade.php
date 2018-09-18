4d27daaa15a3553bac15ebc17a52af80dc4b0455
Abhishek Ahirwar <abhishek.a@portphillippublishing.com.au>
2018-09-18 13:30:33 +1000

    add theme

---
+++ resources/views/layout/header.blade.php
@@ -0,0 +1,21 @@
+ <!-- header begin -->
+<header>
+    <div class="container">
+        <div class="row">
+            <div class="col-md-12">
+                <!-- logo begin -->
+                <div id="logo">
+                    <a href="index.html">
+                        <img class="logo" src="images/logo.png" alt="">
+                    </a>
+                </div>
+                <!-- logo close -->
+                @include('layout.nav')
+
+            </div>
+            <!-- mainmenu close -->
+
+        </div>
+    </div>
+</header>
+<!-- header close -->
