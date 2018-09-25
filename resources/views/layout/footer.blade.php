237e7bcdd1170755852b782475aba917ed52aa54
Abhishek Ahirwar <abhishek.a@portphillippublishing.com.au>
2018-09-25 13:58:06 +1000

    change footer heading

--- resources/views/layout/footer.blade.php
+++ resources/views/layout/footer.blade.php
@@ -5,7 +5,7 @@
                     <div class="row">
                         <div class="col-md-2">
                             <div class="widget">
-                                <h5>{{ strtoupper('Information') }}</h5>
+                                <h5 class="sky-blue-text">{{ strtoupper('Information') }}</h5>
                                 <ul>
                                     <li ><a class="{{ Request::path() == 'about' ? 'active' : '' }}" href="{{ url('about') }}">About IG Blinds</a></li>
                                     <li><a class="{{ Request::path() == 'products' ? 'active' : '' }}" href="{{ url('products') }}">Product Range</a></li>
@@ -16,7 +16,7 @@
                         </div>
                         <div class="col-md-2">
                             <div class="widget">
-                                <h5>{{ strtoupper('CUSTOMER SERVICE') }}</h5>
+                                <h5 class="sky-blue-text">{{ strtoupper('CUSTOMER SERVICE') }}</h5>
                                 <ul>
                                     <li><a class="{{ Request::path() == 'warranty' ? 'active' : '' }}" href="{{ url('warranty') }}">Warranty</a></li>
                                     <li><a class="{{ Request::path() == 'warranty' ? 'active' : '' }}" href="{{ url('faq') }}">FAQs</a></li>
@@ -26,7 +26,7 @@
                         </div>
                         <div class="col-md-2">
                             <div class="widget">
-                                <h5>{{ strtoupper('IG BLINDS PTY LTD') }}</h5>
+                                <h5 class="sky-blue-text">{{ strtoupper('IG BLINDS PTY LTD') }}</h5>
                                 <p>
                                     Unit 10/15 Industrial Ave <br>
                                     Molendinar QLD 4214<br>
@@ -38,7 +38,7 @@
                         </div>
                         <div class="col-md-6">
                             <div class="widget">
-                                <h5>{{ strtoupper('OUR INDUSTRY PARTNERS') }}</h5>
+                                <h5 class="sky-blue-text">{{ strtoupper('OUR INDUSTRY PARTNERS') }}</h5>
                                 <div class="partner-logos">
                                     <img src="images/partner/partner1.png" class="partner-logo picframe">
                                     <img src="images/partner/partner2.png" class="partner-logo picframe">
