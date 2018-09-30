9406110a00843c31e7782e999a77e5b29a3d3e70
Abhishek Ahirwar <abhiahirwar@gmail.com>
2018-10-01 00:35:58 +1000

    spec page

--- resources/views/spec.blade.php
+++ resources/views/spec.blade.php
@@ -5,22 +5,126 @@
   	<!-- section begin -->
     <section id="section-about">
         <div class="container">
-  			<h1>{{ strtoupper(Request::path()) }} IG Blinds ─</h1>
+  			<h1>Colour and Specifications ─</h1>
   			<div class="spacer-double"></div>
   			<div class="row">
-  				 <div class="spacer-double"></div><div class="spacer-double"></div><div class="spacer-double"></div><div class="spacer-double"></div>
+  				 <div class="col-md-8 featured-content">
+            <p>
+              Recognised as one of the most advanced integrated blind systems available.
+            </p>
+            <p>
+              IG Blinds® offer an extensive range of products, designed to meet every need related to shading, colour selection, screening and advanced operating systems.
+            </p>
+          </div>
   			</div>
-  			<div class="spacer-single"></div>
         </div>
     </section>
     <!-- section close -->
 
 
 	<!-- section begin -->
-    <section id="section-testimonial" class="text-light">
+    <section id="" class="">
         <div class="container">
-            
-           <div class="spacer-double"></div><div class="spacer-double"></div><div class="spacer-double"></div><div class="spacer-double"></div>
+          <div class="row">
+            <div class="col-md-6">
+              <h2 class="heading-bar">IG Venetian blind colours</h2>
+              <table class="spec-table blind-spec-table">
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th></th>
+                    <th>Energy reflection</th>
+                    <th>Light reflection</th>
+                    <th>Energy absorption</th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>002</td><td><span class="color-circle bg-color-002"></span></td><td>71%</td><td>79%</td><td>29%</td>
+                  </tr>
+                  <tr>
+                    <td>028</td><td><span class="color-circle bg-color-028"></span></td><td>71%</td><td>79%</td><td>29%</td>
+                  </tr>
+                  <tr>
+                    <td>029</td><td><span class="color-circle bg-color-029"></span></td><td>71%</td><td>79%</td><td>29%</td>
+                  </tr>
+                  <tr>
+                    <td>048</td><td><span class="color-circle bg-color-048"></span></td><td>71%</td><td>79%</td><td>29%</td>
+                  </tr>
+                  <tr>
+                    <td>050</td><td><span class="color-circle bg-color-050"></span></td><td>71%</td><td>79%</td><td>29%</td>
+                  </tr>
+                  <tr>
+                    <td>051</td><td><span class="color-circle bg-color-051"></span></td><td>71%</td><td>79%</td><td>29%</td>
+                  </tr>
+                  <tr>
+                    <td>052</td><td><span class="color-circle bg-color-052"></span></td><td>71%</td><td>79%</td><td>29%</td>
+                  </tr>
+                  <tr>
+                    <td>053</td><td><span class="color-circle bg-color-053"></span></td><td>71%</td><td>79%</td><td>29%</td>
+                  </tr>
+                  <tr>
+                    <td>056</td><td><span class="color-circle bg-color-056"></span></td><td>71%</td><td>79%</td><td>29%</td>
+                  </tr>
+                  <tr>
+                    <td>045</td><td><span class="color-circle bg-color-045"></span></td><td>71%</td><td>79%</td><td>29%</td>
+                  </tr>
+
+                </tbody>
+              </table>
+               <div class="spacer-double"></div>
+            </div>
+            <div class="col-md-6">
+              <h2 class="heading-bar">IG Single pleated colours</h2>
+              <table class="spec-table ">
+                <thead>
+                  <tr>
+                    <th></th>
+                    <th></th>
+                    <th></th>
+                    <th>White </th>
+                    <th>other Colours </th>
+                  </tr>
+                </thead>
+                <tbody>
+                  <tr>
+                    <td>000</td><td><span class="color-circle bg-color-002"></span></td><td class="light-text">Light reflection <br>Light transmission<br> Light absorption</td><td><strong>45-46%</strong></td><td><strong>28-48%</strong></td>
+                  </tr>
+                  <tr>
+                    <td>010</td><td><span class="color-circle bg-color-028"></span></td><td colspan="3">The vanity fabrics have a uniform colour throughout the fabric. The internal and external surfaces have the same colour.</td>
+                  </tr>
+                  <tr>
+                    <td>022</td><td><span class="color-circle bg-color-029"></span></td><td colspan="3"></td>
+                  </tr>
+
+                </tbody>
+              </table>
+              <div class="spacer-double"></div>
+              <h2 class="heading-bar">IG Pleated Sunblock Colours</h2>
+              <table class="spec-table ">
+                <tbody>
+                  <tr>
+                    <td>0201</td><td><span class="color-circle bg-color-0201"></span></td><td class="light-text">Energy reflection <br>Light reflection<br> Energy absorption</td><td><strong>60%<br>72%<br>40%</strong>
+                  </tr>
+                  <tr>
+                    <td>4806</td><td><span class="color-circle bg-color-4806"></span></td><td>Energy transmission<br>Light transmission</td><td><strong>0%<br>0%</strong></td>
+                  </tr>
+                </tbody>
+              </table>
+              <div class="spacer-double"></div>
+              <h2 class="heading-bar">IG Sunveil Colours</h2>
+              <table class="spec-table ">
+                <tbody>
+                  <tr>
+                    <td>0201</td><td><span class="color-circle bg-color-0201"></span></td><td class="light-text">Energy reflection <br>Light reflection<br> Energy absorption</td><td><strong>60%<br>72%<br>40%</strong>
+                  </tr>
+                  <tr>
+                    <td>4440</td><td><span class="color-circle bg-color-4440"></span></td><td>Energy transmission<br>Light transmission</td><td><strong>0%<br>0%</strong></td>
+                  </tr>
+                </tbody>
+              </table>
+            </div>
+          </div>
         </div>
     </section>
     <!-- section close -->
